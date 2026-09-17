b69cc4491d1daf12b4c83fcbc5201118ebf182cb
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-09-17 13:04:57 +0530

    feat: Merge jobs with their respective stars and score
    Fixes #1

--- process.py
+++ process.py
@@ -35,3 +35,24 @@ def process_file(filepath: Path):
         for user in loaded:
             for KEY in DEL_KEYS:
                 del user[KEY]
+
+            job_ids = user["jobs"]
+            updated_jobs = []
+            for job_id in job_ids:
+                try:
+                    job = {"id": job_id}
+                    job["stars"] = user[f"stars_{int(job_id)}"]
+                    job["score"] = user[f"score_{int(job_id)}"]
+                    del user[f"stars_{int(job_id)}"]
+                    del user[f"score_{int(job_id)}"]
+                    updated_jobs.append(job)
+                except KeyError:
+                    pass
+
+            user["jobs"] = updated_jobs
+            copy = user.copy()
+            for key in user:
+                if str(key).startswith("score_") or str(key).startswith("stars_"):
+                    del copy[key]
+
+            user = copy.copy()
