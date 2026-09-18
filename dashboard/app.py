7eb714e697ae5c04f372ed5d54378a61efefef97
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-09-18 16:00:39 +0530

    fix(dashboard): use modern app.run instead of obsolete app.run_server

--- dashboard/app.py
+++ dashboard/app.py
@@ -107,4 +107,4 @@ app.layout = html.Div(
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=8050, debug=False)
